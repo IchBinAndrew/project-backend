20083f181688254a23953a22f6046a41417cd966
Andrew <hello@example.com>
2025-07-13 22:40:34 +0400

    Fixed critical issue with tables being not created on the first launch of application.

--- tasks-api/main.py
+++ tasks-api/main.py
@@ -3,6 +3,7 @@ from uploads_router import router as uploads
 from list_router import router as list_router
 from assign_router import router as assign_router
 from faststream.kafka import KafkaBroker
+from database.models import async_main
 import uvicorn
 
 # broker = KafkaBroker(bootstrap_servers="localhost:9092")
@@ -15,6 +16,10 @@ app = FastAPI()
 app.include_router(uploads, prefix="/task")
 app.include_router(list_router, prefix="/task")
 app.include_router(assign_router, prefix="/task")
+
+@app.on_event("startup")
+async def on_startup():
+    await async_main()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
